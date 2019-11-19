6cf4b5a91d6dd80a7aee96fb7de60281be8446ba
liuyanghong <liuyh@iotsh.com.cn>
2019-11-19 18:09:41 +0800

    ini notes manager user server

---
+++ user-server-control/src/main/java/com/notes/userservercontrol/UserServerControlApplication.java
@@ -0,0 +1,13 @@
+package com.notes.userservercontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServerControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerControlApplication.class, args);
+    }
+
+}
